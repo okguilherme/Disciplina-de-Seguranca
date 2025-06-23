5792dc2a404e5a0cd1482a05a7139bf8cd4fdc6e
wiliene <willienes49@gmail.com>
2025-06-23 18:59:12 -0300

    Keys.py

--- Keys.py
+++ Keys.py
@@ -1,10 +1,12 @@
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives import serialization
 
-# 1) Gerar chave privada
+#Gerando par de chaves ECDSA e as salvando em um arquivo .pem
+
+# Gerar chave privada
 private_key = ec.generate_private_key(ec.SECP256R1())
 
-# 2) Serializar e salvar a chave privada em PEM
+# chave privada em PEM
 with open("private_key.pem", "wb") as f:
     f.write(
         private_key.private_bytes(
@@ -14,7 +16,7 @@ with open("private_key.pem", "wb") as f:
         )
     )
 
-# 3) Gerar chave pública e salvar em PEM
+# chave pública e salvar em PEM
 public_key = private_key.public_key()
 with open("public_key.pem", "wb") as f:
     f.write(
